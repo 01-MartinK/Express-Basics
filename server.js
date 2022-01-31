800e16af73d14d08dc603a2851c8acbb66155072
Martin K <thedeadgamer101@gmail.com>
2022-01-31 16:51:35 +0200

    templating directory setup

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 // public directory
 app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res) => {
   let user = req.params.username;
@@ -9,6 +14,6 @@ app.get('/user/:username', (req, res) => {
   res.render('index.ejs', {username: user});
 });
 
-app.listen(3010, ()=> {
+app.listen(3011, ()=> {
   console.log('Server started on http://localhost:8080');
 });
