2cb1ac5c518e0fd568804fc3c6280851fff2e5d8
Martin K <thedeadgamer101@gmail.com>
2022-01-31 16:41:43 +0200

    Basic use of ejs

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<a href="/contact">Contact Us </a> <br> <a href="/about">About Us</a>');
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.send('<h1>Contact Us Page</h1>');
-  });
-  
-  app.get('/about', (req, res) => {
-    res.send('<h1>About Us Page</h1>');
-  });
-  
-  app.get('*', (req, res) => {
-    res.send('404. Tis page does not exist. <a href="/">Go to HomePage</a>');
-  });
-  
-  app.listen(8080, ()=> {
-    console.log('Server started on http://localhost:8080')
-  });
+app.get('/user/:username', (req, res) => {
+  let user = req.params.username;
+
+  res.render('index.ejs', {username: user});
+});
+
+app.listen(8080, ()=> {
+  console.log('Server started on http://localhost:8080');
+});
