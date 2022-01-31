d2cec9383e8b81c5a54e681486f739ccc3aa9339
Martin K <thedeadgamer101@gmail.com>
2022-01-31 17:07:01 +0200

    post request use

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
 // public directory
 app.use(express.static('public'));
 
+// viewing
 app.set('views', path.join(__dirname, 'views'))
-
 app.set('view engine', 'ejs');
 
-app.get('/user/:username', (req, res) => {
-  let user = req.params.username;
-
-  res.render('index.ejs', {username: user});
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-app.listen(3011, ()=> {
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(username == 'user' && password === 'qwerty'){
+    res.redirect('/dashboard');
+  } 
+
+  console.log('post request done');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+app.listen(3000, ()=> {
   console.log('Server started on http://localhost:8080');
 });
